f27db7e905a733ef754aa934f1c8c72372745802
Nelson <nelson.guerreiro@nc-services.fr>
2023-07-28 01:09:51 +0200

    theming2

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,5 @@
+import { Lato_400Regular, useFonts as useLato } from '@expo-google-fonts/lato';
+import { Oswald_400Regular, useFonts as useOswald } from '@expo-google-fonts/oswald';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
@@ -6,14 +8,24 @@ import { ItemsScreen } from './src/features/Items/screens/ItemsScreen';
 import { theme } from './src/infrastructure/theme';
 
 export default App = () => {
-    return (
-        <>
-            <ThemeProvider theme={theme}>
-                <ItemsScreen />
-            </ThemeProvider>
-            <ExpoStatusBar style='auto' />
-        </>
-    );
+	const [fontsLoaded] = useLato({
+		Lato_400Regular,
+	});
+	const [fontsLoaded2] = useOswald({
+		Oswald_400Regular,
+	});
+	if (!fontsLoaded || !fontsLoaded2) {
+		return null;
+	}
+
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<ItemsScreen />
+			</ThemeProvider>
+			<ExpoStatusBar style="auto" />
+		</>
+	);
 };
 
 const styles = StyleSheet.create({});
--- frontend/src/features/Items/components/ItemCard.js
+++ frontend/src/features/Items/components/ItemCard.js
@@ -5,28 +5,37 @@ import { styled } from 'styled-components/native';
 
 // const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 const Title = styled.Text`
-    padding: ${props => props.theme.space[3]};
-    text-align: center;
-    font-weight: bold;
-    font-size: ${props => props.theme.fontSizes[2]};
-    color: ${props => props.theme.primary};
+	font-family: ${(props) => props.theme.fonts.body};
+	padding: ${(props) => props.theme.space[3]};
+	text-align: center;
+	font-weight: ${(props) => props.theme.fontWeights.bold};
+	font-size: ${(props) => props.theme.fontSizes[2]};
+	color: ${(props) => props.theme.colors.brand.primary};
 `;
-
+const Description = styled.Text`
+	font-family: ${(props) => props.theme.fonts.body};
+	padding: ${(props) => props.theme.space[3]};
+	text-align: center;
+	font-weight: ${(props) => props.theme.fontWeights.regular};
+	font-size: ${(props) => props.theme.fontSizes[1]};
+	color: ${(props) => props.theme.colors.brand.primary};
+`;
 const ItemCards = styled(Card)`
-    background-color: ${props => props.theme.colors.primary};
+	background-color: ${(props) => props.theme.colors.brand.primary};
 `;
 
 const ItemCardCover = styled(Card.Cover)`
-    padding: ${props => props.theme.space[3]};
-    background-color: white;
+	padding: ${(props) => props.theme.space[3]};
+	background-color: white;
 `;
 
-export const ItemCard = props => {
-    console.log(props.item.description.details);
-    return (
-        <ItemCards>
-            <ItemCardCover source={{ uri: props.item.description.photos[0] }} />
-            <Title>{props.item.title}</Title>
-        </ItemCards>
-    );
+export const ItemCard = (props) => {
+	console.log(props.item.description.details);
+	return (
+		<ItemCards>
+			<ItemCardCover source={{ uri: props.item.description.photos[0] }} />
+			<Title>{props.item.title}</Title>
+			<Description>{props.item.description.details}</Description>
+		</ItemCards>
+	);
 };
