237aa77f3ccdd356e7627a3db15aa45416fe0453
Nelson <nelson.guerreiro@nc-services.fr>
2023-07-28 02:19:59 +0200

    dodo

--- frontend/src/features/Items/components/ItemCard.js
+++ frontend/src/features/Items/components/ItemCard.js
@@ -1,41 +1,65 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { Card } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
 import { styled } from 'styled-components/native';
-
-// const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
-const Title = styled.Text`
+import star from '../../../../assets/star';
+const Info = styled.View`
+	padding: ${(props) => props.theme.space[2]};
+`;
+const Address = styled.Text`
 	font-family: ${(props) => props.theme.fonts.body};
-	padding: ${(props) => props.theme.space[3]};
-	text-align: center;
+	font-size: ${(props) => props.theme.fontSizes.caption};
+	padding: ${(props) => props.theme.space[2]};
+	font-weight: ${(props) => props.theme.fontWeights.regular};
+`;
+
+const Title = styled.Text`
+	font-family: ${(props) => props.theme.fonts.heading};
+	padding: ${(props) => props.theme.space[2]};
 	font-weight: ${(props) => props.theme.fontWeights.bold};
-	font-size: ${(props) => props.theme.fontSizes[2]};
-	color: ${(props) => props.theme.colors.brand.primary};
+	font-size: ${(props) => props.theme.fontSizes.title};
+	color: ${(props) => props.theme.colors.text.primary};
 `;
 const Description = styled.Text`
 	font-family: ${(props) => props.theme.fonts.body};
-	padding: ${(props) => props.theme.space[3]};
-	text-align: center;
+	padding: ${(props) => props.theme.space[2]};
 	font-weight: ${(props) => props.theme.fontWeights.regular};
-	font-size: ${(props) => props.theme.fontSizes[1]};
-	color: ${(props) => props.theme.colors.brand.primary};
+	font-size: ${(props) => props.theme.fontSizes.body};
+	color: ${(props) => props.theme.colors.text.primary};
 `;
+
+const Rating = styled.View`
+	flex-direction: row;
+	padding: ${(props) => props.theme.space[2]};
+`;
+
 const ItemCards = styled(Card)`
-	background-color: ${(props) => props.theme.colors.brand.primary};
+	background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const ItemCardCover = styled(Card.Cover)`
-	padding: ${(props) => props.theme.space[3]};
-	background-color: white;
+	padding: ${(props) => props.theme.space[2]};
+	background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 export const ItemCard = (props) => {
 	console.log(props.item.description.details);
+	const ratingArray = Array.from(new Array(Math.ceil(props.item.rating)));
 	return (
 		<ItemCards>
 			<ItemCardCover source={{ uri: props.item.description.photos[0] }} />
-			<Title>{props.item.title}</Title>
+			<Info>
+				<Title>{props.item.name}</Title>
+				<Rating>
+					{ratingArray.map((index) => (
+						<SvgXml xml={star} width={20} height={20} />
+					))}
+				</Rating>
+				<Address>A 1 km</Address>
+			</Info>
 			<Description>{props.item.description.details}</Description>
+			<Address>Prix/Jour 45 $</Address>
 		</ItemCards>
 	);
 };
--- frontend/src/features/Items/screens/ItemsScreen.js
+++ frontend/src/features/Items/screens/ItemsScreen.js
@@ -5,49 +5,50 @@ import { styled } from 'styled-components/native';
 import { ItemCard } from '../components/ItemCard';
 
 const SafeArea = styled(SafeAreaView)`
-    flex: 1;
-    margin-top: ${props => props.theme.space[3]};
-    margin-top: ${StatusBar.currentHeight ? `${StatusBar.currentHeight}px` : '20px'};
+	flex: 1;
+	margin-top: ${(props) => props.theme.space[3]};
+	margin-top: ${StatusBar.currentHeight ? `${StatusBar.currentHeight}px` : '20px'};
 `;
 
 const SearchContainer = styled.View`
-    padding: ${props => props.theme.space[3]};
+	padding: ${(props) => props.theme.space[3]};
 `;
 
 const ItemListContainer = styled.View`
-    flex: 1;
-    padding: ${props => props.theme.space[3]};
+	flex: 1;
+	padding: ${(props) => props.theme.space[3]};
 `;
 export const ItemsScreen = () => {
-    const items = {
-        name: 'Souris',
-        description: {
-            dimensions: { hauteur: 12, largeur: 12, poids: 1 },
-            etat: 'neuf',
-            details: 'Souris du futur, qui parle',
-            photos: ['https://www.zerker.ca/misc/forumstuff/DSC00669.JPG', 'https://www.zerker.ca/misc/forumstuff/DSC00669.JPG', 'img3.jpg'],
-            videos: ['mov1.mp4', 'mov2.mp4', 'mov3.mp4'],
-        },
-        ownerUserId: '1234567890',
-        categoryId: '1234567890',
-        prices: {
-            perHour: 100,
-            perDay: 200,
-            perWeek: 700,
-            perMonth: 2000,
-        },
-        cautions: 450,
-        isAvailable: true,
-        positionRange: [100],
-    };
-    return (
-        <SafeArea>
-            <SearchContainer>
-                <Searchbar />
-            </SearchContainer>
-            <ItemListContainer>
-                <ItemCard item={items} />
-            </ItemListContainer>
-        </SafeArea>
-    );
+	const items = {
+		name: 'Souris',
+		description: {
+			dimensions: { hauteur: 12, largeur: 12, poids: 1 },
+			etat: 'neuf',
+			details: 'Souris du futur, qui parle',
+			photos: ['https://www.zerker.ca/misc/forumstuff/DSC00669.JPG', 'https://www.zerker.ca/misc/forumstuff/DSC00669.JPG', 'img3.jpg'],
+			videos: ['mov1.mp4', 'mov2.mp4', 'mov3.mp4'],
+		},
+		ownerUserId: '1234567890',
+		categoryId: '1234567890',
+		prices: {
+			perHour: 100,
+			perDay: 200,
+			perWeek: 700,
+			perMonth: 2000,
+		},
+		cautions: 450,
+		isAvailable: true,
+		positionRange: [100],
+		rating: 4.5,
+	};
+	return (
+		<SafeArea>
+			<SearchContainer>
+				<Searchbar />
+			</SearchContainer>
+			<ItemListContainer>
+				<ItemCard item={items} />
+			</ItemListContainer>
+		</SafeArea>
+	);
 };
