97ed347c2ffd86631b81fb7f25c46e3304d68f01
Nelson <nelson.guerreiro@nc-services.fr>
2023-08-10 19:28:43 +0200

    ave before nav

--- frontend/src/features/Items/components/MapPicker.js
+++ frontend/src/features/Items/components/MapPicker.js
@@ -1,6 +1,6 @@
 import * as Location from 'expo-location';
 import React, { useEffect, useState } from 'react';
-import { Button, Modal, View } from 'react-native';
+import { Button, KeyboardAvoidingView, Modal, View } from 'react-native';
 import Geocoder from 'react-native-geocoding';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import MapView, { Marker } from 'react-native-maps';
@@ -63,48 +63,50 @@ export const MapPicker = ({ onLocationSelected, isVisible, onClose }) => {
 
 	return (
 		<Modal visible={isVisible} onRequestClose={onClose}>
-			<View style={{ flex: 1 }}>
-				<MapView style={{ flex: 1 }} region={region}>
-					{location && <Marker coordinate={location} draggable onDragEnd={handleMarkerDragEnd} />}
-				</MapView>
-				<View style={{ height: 120 }}>
-					<GooglePlacesAutocomplete
-						placeholder="Search"
-						fetchDetails={true}
-						onPress={(data, details = null) => {
-							const newRegion = {
-								latitude: details.geometry.location.lat,
-								longitude: details.geometry.location.lng,
-								latitudeDelta: 0.0922,
-								longitudeDelta: 0.0421,
-							};
-							setLocation(newRegion);
-							setRegion(newRegion);
-							setAddress(details.formatted_address);
-						}}
-						query={{
-							key: 'AIzaSyCKVV2S52hUifM6pOSiTVzj2MoAI4jccqw',
-							language: 'en',
-						}}
-						styles={{
-							textInputContainer: {
-								height: 40, // Or set this to the desired height
-								borderTopWidth: 0,
-								borderBottomWidth: 0,
-							},
-							textInput: {
-								marginLeft: 0,
-								marginRight: 0,
-								height: 38, // The height of the TextInput is slightly smaller than the container
-								color: '#5d5d5d',
-								fontSize: 16,
-							},
-						}}
-					/>
+			<KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+				<View style={{ flex: 1 }}>
+					<MapView style={{ flex: 1 }} region={region}>
+						{location && <Marker coordinate={location} draggable onDragEnd={handleMarkerDragEnd} />}
+					</MapView>
+					<View style={{ height: 120 }}>
+						<GooglePlacesAutocomplete
+							placeholder="Search"
+							fetchDetails={true}
+							onPress={(data, details = null) => {
+								const newRegion = {
+									latitude: details.geometry.location.lat,
+									longitude: details.geometry.location.lng,
+									latitudeDelta: 0.0922,
+									longitudeDelta: 0.0421,
+								};
+								setLocation(newRegion);
+								setRegion(newRegion);
+								setAddress(details.formatted_address);
+							}}
+							query={{
+								key: 'AIzaSyCKVV2S52hUifM6pOSiTVzj2MoAI4jccqw',
+								language: 'en',
+							}}
+							styles={{
+								textInputContainer: {
+									height: 40, // Or set this to the desired height
+									borderTopWidth: 0,
+									borderBottomWidth: 0,
+								},
+								textInput: {
+									marginLeft: 0,
+									marginRight: 0,
+									height: 38, // The height of the TextInput is slightly smaller than the container
+									color: '#5d5d5d',
+									fontSize: 16,
+								},
+							}}
+						/>
+					</View>
+					<TextInput label="Address" value={address} onChangeText={(text) => setAddress(text)} />
+					<Button title="Submit" onPress={handleSubmit} />
 				</View>
-				<TextInput label="Address" value={address} onChangeText={(text) => setAddress(text)} />
-				<Button title="Submit" onPress={handleSubmit} />
-			</View>
+			</KeyboardAvoidingView>
 		</Modal>
 	);
 };
